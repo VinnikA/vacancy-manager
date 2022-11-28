e6691ca623010686ceee623101ba2ce86eba3f4e
VinnikA <vamposht@gmail.com>
2022-11-28 16:52:49 +0200

    fix: after error

--- src/components/example-card/index.tsx
+++ src/components/example-card/index.tsx
@@ -5,10 +5,9 @@ export default function ExampeCard() {
     <div className="border rounded-lg w-[300px] mt-4 mx-auto p-4 bg-white text-slate-700">
       <h2 className="font-bold text-3xl text-center mb-4">Heading</h2>
       <p className="mb-4">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
-        voluptatibus deleniti quis error eaque unde, ratione debitis explicabo
-        dicta aliquam suscipit! Obcaecati, iste a? Molestiae repudiandae et
-        aliquam perspiciatis suscipit?
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero voluptatibus deleniti quis error eaque unde,
+        ratione debitis explicabo dicta aliquam suscipit! Obcaecati, iste a? Molestiae repudiandae et aliquam
+        perspiciatis suscipit?
       </p>
       <div className="flex gap-4">
         <button type="button" className="btn bg-slate-700 text-white">
