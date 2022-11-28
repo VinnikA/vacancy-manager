fc547be026020d7f17cc419ef0d0577628a4bd82
VinnikA <vamposht@gmail.com>
2022-11-28 10:52:37 +0200

    feat: example-card added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import ExampeCard from './components/example-card';
+
 function App() {
-  return <div className="app"> Vacancy Manager </div>;
+  return (
+    <div className="app">
+      <ExampeCard />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/example-card/index.tsx
@@ -0,0 +1,23 @@
+import './styles.css';
+
+export default function ExampeCard() {
+  return (
+    <div className="border rounded-lg w-[300px] mt-4 mx-auto p-4 bg-white text-slate-700">
+      <h2 className="font-bold text-3xl text-center mb-4">Heading</h2>
+      <p className="mb-4">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero
+        voluptatibus deleniti quis error eaque unde, ratione debitis explicabo
+        dicta aliquam suscipit! Obcaecati, iste a? Molestiae repudiandae et
+        aliquam perspiciatis suscipit?
+      </p>
+      <div className="flex gap-4">
+        <button type="button" className="btn bg-slate-700 text-white">
+          Add
+        </button>
+        <button type="button" className="btn">
+          Info
+        </button>
+      </div>
+    </div>
+  );
+}
