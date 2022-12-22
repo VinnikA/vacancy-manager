b1b763a63b48bc3ca3751a6aabc4376b9665ba93
VinnikA <vamposht@gmail.com>
2022-12-23 00:09:40 +0200

    work with routing started

---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Sign Up Page</h1>
+    </div>
+  );
+}
+
+export default SignUp;
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.css';
+
+function UserMainPage() {
+  return (
+    <div>
+      <h1>hello user</h1>
+    </div>
+  );
+}
+
+export default UserMainPage;
